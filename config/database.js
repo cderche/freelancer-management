125c1ee3fa5f3e86ec27b683df28e45ef4998707
cderche <Kirill@MacBook.local>
2016-07-30 02:14:56 +0300

    Trying to bundle again.

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
   var mongoose = require('mongoose');
-  console.log(`MONGODB_URL ${process.env.MONGODB_URI}`);
+  console.log(`MONGODB_URI: ${process.env.MONGODB_URI}`);
   mongoose.connect(process.env.MONGODB_URI)
   // mongoose.connect('mongodb://heroku_jl48rsp8:5s3uq3dh5kvnlf939bf8o2repk@ds029575.mlab.com:29575/heroku_jl48rsp8')
 
