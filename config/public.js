ae413e6a66026a602db3db1b62412c3d71c75e45
cderche <Kirill@MacBook.local>
2016-07-30 02:37:16 +0300

    public dir change

--- config/public.js
+++ config/public.js
@@ -1,6 +1,7 @@
 module.exports = function(app) {
   var express = require('express')
   var path = require('path');
-  app.use(express.static(path.join(__dirname, '../.tmp/public')));
+  // app.use(express.static(path.join(__dirname, '../.tmp/public')));
+  app.use(express.static(path.join(__dirname, '../public')));
   // app.use(express.static('../public'));
 }
