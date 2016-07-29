16b8687329c06cf4910d36e2c2557921f76c4003
cderche <Kirill@MacBook.local>
2016-07-30 00:22:44 +0300

    Fixed DB connection

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
   var mongoose = require('mongoose');
-  // mongoose.connect(process.env.MONGODB_URL)
-  mongoose.connect('mongodb://heroku_jl48rsp8:5s3uq3dh5kvnlf939bf8o2repk@ds029575.mlab.com:29575/heroku_jl48rsp8')
+  mongoose.connect(process.env.MONGODB_URL)
+  // mongoose.connect('mongodb://heroku_jl48rsp8:5s3uq3dh5kvnlf939bf8o2repk@ds029575.mlab.com:29575/heroku_jl48rsp8')
 
   var db = mongoose.connection
   db.on('error', console.error.bind(console, 'connection error:'));
