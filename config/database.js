fa427d31ff33be32232b42406451c0bb4738dd63
cderche <Kirill@MacBook.local>
2016-07-30 09:46:07 +0300

    Migrate database config

---
+++ config/database.js
@@ -0,0 +1,11 @@
+module.exports = function(app) {
+  var mongoose = require('mongoose');
+
+  mongoose.connect(process.env.MONGODB_URI)
+
+  var db = mongoose.connection
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    console.log(`we're connected!`)
+  });
+}
