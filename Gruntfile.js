282fea6f269d8ff6b24e05f92d10c2906de021df
cderche <Kirill@MacBook.local>
2016-07-30 03:20:03 +0300

    Grunt copy & gentelella

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,69 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd:    'public/stylesheets',
+          src:    ['*.scss'],
+          dest:   '.tmp/public/stylesheets',
+          ext:    '.min.css'
+        }]
+      }
+    },
+    copy: {
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd:    'node_modules/gentelella/build',
+            src:    ['**/*'],
+            dest:   '.tmp/public/gentelella/build',
+            // ext:    '.min.js'
+          },
+          {
+            expand: true,
+            cwd:    'node_modules/gentelella/vendors',
+            src:    ['**/*'],
+            dest:   '.tmp/public/',
+            // ext:    '.min.js'
+          },
+          {
+            expand: true,
+            cwd: 'node_modules/gentelella/vendors/font-awesome/fonts/',
+            src: ['*'],
+            dest: '.tmp/public/fonts/', filter: 'isFile'
+          },
+          {
+            expand: true,
+            cwd:    'public/javascripts',
+            src:    ['*.js'],
+            dest:   '.tmp/public/js',
+          },
+          {
+            expand: true,
+            cwd:    'public',
+            src:    ['*.pdf'],
+            dest:   '.tmp/public'
+          },
+          {
+            expand: true,
+            cwd:    'public/images',
+            src:    ['**/*'],
+            dest:   '.tmp/public/images'
+          },
+        ]
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
+  grunt.registerTask('default', [
+    'sass',
+    'copy'
+  ]);
 };
--- config/public.js
+++ config/public.js
@@ -3,6 +3,5 @@ var path = require('path');
 
 module.exports = function(app) {
   console.log('Configuring public.');
-  app.use(express.static(path.join(__dirname, '../public')));
-  // app.use(express.static('../public'));
+  app.use(express.static(path.join(__dirname, '.tmp/public')));
 }
